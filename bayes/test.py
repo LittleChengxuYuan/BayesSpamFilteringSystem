110272245e22b3a536105a3dae1fddec01d0cfc7
LittleChengxuYuan <quyongchuang@outlook.com>
2018-04-18 15:32:47 +0800

    添加中文支持，并将停用词表合并

--- bayes/test.py
+++ bayes/test.py
@@ -1,9 +1,13 @@
 from Bayes import Bayes
 import os
 import re
-testFilePath = "./test/test-mails"
+testFilePath = "./test/English/test-mails/ham"
 testFileList = os.listdir(testFilePath)
-bayes = Bayes("./test/train-mails/ham","./test/train-mails/spam","./stopwords/StopWordsENG.txt")
+bayes = Bayes("./test/English/train-mails/ham","./test/English/train-mails/spam","./stopwords/StopWords.txt")
+isSpam = 0
 for fileName in testFileList:
-    print(fileName)
-    bayes.getResult(testFilePath+'/'+fileName)
+    #print(fileName)
+    result = 0
+    result = bayes.getResult(testFilePath+'/'+fileName)
+    if result==0:
+        isSpam = isSpam + 1
